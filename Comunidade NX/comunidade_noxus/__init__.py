cfb17e0871b621697cfde26d90911249babd4ee3
Gabriel Rodrigues Dos Santos <158094898+gabriellsantos485@users.noreply.github.com>
2024-08-01 12:21:14 -0300

    Update __init__.py

--- Comunidade NX/comunidade_noxus/__init__.py
+++ Comunidade NX/comunidade_noxus/__init__.py
@@ -6,7 +6,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b5d39209fbf531b21a5cbd041d6d2947'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ZGwjxcBIHBeMYBMfUyhEuWByBvbFfCEn@roundhouse.proxy.rlwy.net:49312/railway'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 bcrypt=Bcrypt()
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
