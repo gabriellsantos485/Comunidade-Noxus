feafb475c05bb52471c7067b4d501ee0bb1ccb6e
Gabriel Rodrigues Dos Santos <158094898+gabriellsantos485@users.noreply.github.com>
2024-08-01 12:30:48 -0300

    Update __init__.py

--- Comunidade NX/comunidade_noxus/__init__.py
+++ Comunidade NX/comunidade_noxus/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5d39209fbf531b21a5cbd041d6d2947'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///comunidade.db"
 bcrypt=Bcrypt()
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
