e0e53464c8f4532f8a26e416e8467bf297ce2a1d
Gabriel Rodrigues Dos Santos <158094898+gabriellsantos485@users.noreply.github.com>
2024-08-02 02:22:37 -0300

    Update __init__.py

--- Comunidade NX/comunidade_noxus/__init__.py
+++ Comunidade NX/comunidade_noxus/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask                                                                                
+from flask import Flask, render_template, url_for, request, flash , redirect                                                                                    
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -10,11 +12,22 @@ if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///comunidade.db"
+database = SQLAlchemy(app)
 bcrypt=Bcrypt()
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-database=SQLAlchemy(app)
+from comunidade_noxus import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop.all()
+        database.create_all()
+        print("Base de dados criados")
+else:
+    print("Base de dados ja existente")
+
 
 from comunidade_noxus import routes
