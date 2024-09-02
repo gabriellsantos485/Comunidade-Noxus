1fa2eca8229736af45ae78d6089cd7782c3ea77e
Gabriel Rodrigues Dos Santos <158094898+gabriellsantos485@users.noreply.github.com>
2024-09-02 08:49:17 -0300

    Update routes.py

--- Comunidade NX/comunidade_noxus/routes.py
+++ Comunidade NX/comunidade_noxus/routes.py
@@ -41,7 +41,7 @@ def login():
             flash("Email ou senha incorretos, tente novamente")
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        senha_cript=bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_cript=bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario=Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
